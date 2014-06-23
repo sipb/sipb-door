32647ed0b52d989dbd1a41bf0168788bce856cda
Ray Hua Wu <dzaefn@mit.edu>
2014-06-23 19:24:56 -0400

    Added UTC deviation

--- data.py
+++ data.py
@@ -3,12 +3,13 @@ import json,time
 SEC_IN_DAY = 86400
 UNIX_WEEK_DEVIATION = 259200
 DAY_IN_WEEK = 7
+UTC_DEVIATION = 3600
 SEC_IN_WEEK = SEC_IN_DAY * DAY_IN_WEEK
 
 # Get the number of seconds since the most recent 
 # week start (midnight between Sat. and Sun.)
 def seconds_since_week_start(time):
-  return int(time - UNIX_WEEK_DEVIATION) % SEC_IN_WEEK
+  return int(time - UNIX_WEEK_DEVIATION - UTC_DEVIATION) % SEC_IN_WEEK
 
 # Given a list of tuples of open and close times, 
 # return list of tuples of times since first week start
